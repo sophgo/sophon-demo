fbf69af56749243591f383f5143220a1e0849067
longxiang.xu <longxiang.xu@sophgo.com>
2022-10-27 12:26:32 +0800

    fix(RetinaFace): delete LICENSE & unnecessary copyright
    Change-Id: Ia569c6005ae66ba544373f3fbcd3500de4634d72

--- sample/RetinaFace/tools/widerface_evaluate/setup.py
+++ sample/RetinaFace/tools/widerface_evaluate/setup.py
@@ -1,11 +1,4 @@
-#===----------------------------------------------------------------------===#
-#
-# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
-#
-# SOPHON-DEMO is licensed under the 2-Clause BSD License except for the
-# third-party components.
-#
-#===----------------------------------------------------------------------===#
+
 """
 WiderFace evaluation code
 author: wondervictor
