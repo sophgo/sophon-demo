eae44099502cfa03242e9d18269a919029a4c34f
hao.fang01 <hao.fang01@sophgo.com>
2023-02-22 20:40:02 +0800

    fix(PP-OCR):fix download script
    Change-Id: Ie7e9ecd91daac8d01a39d3de4b8ed3cba0eab94a
    (cherry picked from commit 7c848798df0aed6c1fea058c1fc18d5edb68c9c5)
    (cherry picked from commit 790a6d5849c73d096ad7168cb89eff1e8e1d5fa2)

--- sample/PP-OCR/scripts/download.sh
+++ sample/PP-OCR/scripts/download.sh
@@ -14,28 +14,28 @@ tar -xvf ppocr_img.tar -C ../data/images
 rm ppocr_img.tar
 
 # ch_PP-OCRv2_det_infer
-python3 -m dfn --url http://219.142.246.77:65000/sharing/qaPKA38Bz
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/EV43KVuod
 tar -xvf ch_PP-OCRv2_det_infer.tar -C ../data/models/paddle
 rm ch_PP-OCRv2_det_infer.tar
 
 # ch_PP-OCRv2_rec_infer 
-python3 -m dfn --url http://219.142.246.77:65000/sharing/KplOt0kYR
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/Ak6rgWfK5
 tar -xvf ch_PP-OCRv2_rec_infer.tar -C ../data/models/paddle
 rm ch_PP-OCRv2_rec_infer.tar 
 
 # ch_ppocr_mobile_v2.0_cls_infer
-python3 -m dfn --url http://219.142.246.77:65000/sharing/a9ES7hRWg
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/WstzuocWh
 tar -xvf ch_ppocr_mobile_v2.0_cls_infer.tar -C ../data/models/paddle
 rm ch_ppocr_mobile_v2.0_cls_infer.tar
 
 
 # # BM1684
-python3 -m dfn --url http://219.142.246.77:65000/sharing/Dpnpgf9ev
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/Pr2XI8SdF
 tar -xvf BM1684.tar -C ../data/models
 rm BM1684.tar
 
 # # BM1684X
-python3 -m dfn --url http://219.142.246.77:65000/sharing/ChHEwvB3O
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/OTn5VX53j
 tar -xvf BM1684X.tar -C ../data/models
 rm BM1684X.tar 
 
