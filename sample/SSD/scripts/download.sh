035e8288e745533f7b9bffc28f8fb742c97c9c7a
liheng.fang <liheng.fang@sophgo.com>
2023-03-08 17:32:08 +0800

    fix(SSD):update models and download script.
    Change-Id: Ic0fed7bac22d38cc647038347f1435ebebfab9e4

--- sample/SSD/scripts/download.sh
+++ sample/SSD/scripts/download.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 scripts_dir=$(dirname $(readlink -f "$0"))
 # echo $scripts_dir
+# sudo apt-get install zip
 pip3 install dfn
 pushd $scripts_dir
-python3 -m dfn --url http://219.142.246.77:65000/sharing/wsCN1dBbO
-python3 -m dfn --url http://219.142.246.77:65000/sharing/fVyXNs9Eb
-tar -xvf data*.tar.gz -C ../
-rm data*.tar.gz
-mv video.mp4 ../data/
+python3 -m dfn --url http://219.142.246.77:65000/sharing/WWGkMQaca
+unzip data.zip -d ../
+rm data.zip
 echo "All done!"
 popd
