1423d2a82ce13e996e4b9d50bdd84c054396bf8e
jingyu.lu <jingyu.lu@sophgo.com>
2023-03-21 17:13:39 +0800

    feat(YOLOv7):add YOLOv7
    Change-Id: I97194adf79baf0aa57b263e4c39b9bd9a89f9738

---
+++ sample/YOLOv7/scripts/gen_fp32bmodel_mlir.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+model_dir=$(cd `dirname $BASH_SOURCE[0]`/ && pwd)
+
+if [ ! $1 ]; then
+    target=bm1684x
+else
+    target=$1
+fi
+
+outdir=../models/BM1684X
+
+function gen_mlir()
+{
+    model_transform.py \
+        --model_name yolov7_v0.1_3output \
+        --model_def ../models/onnx/yolov7_v0.1_3output_$1b.onnx \
+        --input_shapes [[$1,3,640,640]] \
+        --mlir yolov7_v0.1_3output_$1b.mlir
+}
+
+function gen_fp32bmodel()
+{
+    model_deploy.py \
+        --mlir yolov7_v0.1_3output_$1b.mlir \
+        --quantize F32 \
+        --chip $target \
+        --model yolov7_v0.1_3output_fp32_$1b.bmodel
+
+    mv yolov7_v0.1_3output_fp32_$1b.bmodel $outdir/
+}
+
+pushd $model_dir
+if [ ! -d $outdir ]; then
+    mkdir -p $outdir
+fi
+# batch_size=1
+gen_mlir 1
+gen_fp32bmodel 1
+# batch_size=4
+gen_mlir 4
+gen_fp32bmodel 4
+popd
