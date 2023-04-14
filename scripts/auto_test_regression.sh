5fdf35d1ed28f67d965e0e943cc9cc41f13e7ae2
liheng.fang <liheng.fang@sophgo.com>
2023-04-14 10:17:52 +0800

    feat(auto_test): add dependdencies examination.
    Change-Id: I82b940ae879bae2b5e965a34b326494c7deef8ce

--- scripts/auto_test_regression.sh
+++ scripts/auto_test_regression.sh
@@ -6,13 +6,14 @@ pushd $top_dir
 TARGET="BM1684X"
 MODE="pcie_test"
 TPUID=0
+sail_list=("YOLOv5" "CenterNet") #c++ sail demo
 
 usage() 
 {
-  echo "Usage: $0 [ -m MODE compile_nntc|compile_mlir|pcie_test|soc_build|soc_test] [ -t TARGET BM1684|BM1684X] [ -s SOCSDK] [ -d TPUID]" 1>&2 
+  echo "Usage: $0 [ -m MODE compile_nntc|compile_mlir|pcie_test|soc_build|soc_test] [ -t TARGET BM1684|BM1684X] [ -s SOCSDK] [-a SAIL] [ -d TPUID]" 1>&2 
 }
 
-while getopts ":m:t:s:d:" opt
+while getopts ":m:t:s:a:d:" opt
 do
   case $opt in 
     m)
@@ -24,6 +25,9 @@ do
     s)
       SOCSDK=${OPTARG}
       echo "soc-sdk is $SOCSDK";;
+    a)
+      SAIL_PATH=${OPTARG}
+      echo "sail_path is $SAIL_PATH";;
     d)
       TPUID=${OPTARG}
       echo "using tpu $TPUID";;
@@ -49,7 +53,11 @@ function test_sample(){
         mkdir log_auto_test_regression
     fi
     current_time=$(date +%Y-%m-%d_%H:%M:%S)
-    ./sample/$1/scripts/auto_test.sh -t $TARGET -m $MODE -s $SOCSDK -d $TPUID > ./log_auto_test_regression/$1_$current_time.log 2>&1
+    if [[ " ${sail_list[*]} " == *" $1 "* ]]; then
+      ./sample/$1/scripts/auto_test.sh -t $TARGET -m $MODE -s $SOCSDK -a $SAIL_PATH -d $TPUID > ./log_auto_test_regression/$1_$current_time.log 2>&1
+    else
+      ./sample/$1/scripts/auto_test.sh -t $TARGET -m $MODE -s $SOCSDK -d $TPUID > ./log_auto_test_regression/$1_$current_time.log 2>&1
+    fi
     tail -n 4 ./log_auto_test_regression/$1_$current_time.log | head -n 3
     if test $MODE = "soc_test"
     then
@@ -65,12 +73,49 @@ function test_sample(){
     echo "=============="
 }
 
+function test_dpkg(){
+  package_name=$1
+  if dpkg -s "$package_name" >/dev/null 2>&1; then
+      echo "$package_name is already installed."
+  else
+      echo "$package_name is not installed. Installing now..."
+      sudo apt-get install "$package_name"
+  fi
+}
+
+# Confirm sophon-driver/sophon-libsophon/sophon-mw were previously installed.
+
+# dependencies
+test_dpkg unzip
+test_dpkg p7zip #LPRNet
+test_dpkg p7zip-full
+if [ $MODE = "pcie_test" ] || [ $MODE = "soc_build" ]
+then
+  test_dpkg libeigen3-dev #Trackers
+fi
+
+pip3 install dfn==1.0.2
+if [ $MODE = "pcie_test" ] || [ $MODE = "soc_test" ]
+then
+  pip3 install pycocotools
+  res=$(pip3 list|grep sophon)
+  if [ $? != 0 ];
+  then
+      echo "Please install sail on your system!"
+      exit
+  fi
+fi
+
 test_sample YOLOv5
+test_sample YOLOv5_opt
+test_sample YOLOv7
+test_sample YOLOv8
+test_sample CenterNet
 test_sample C3D
 test_sample ResNet
-test_sample YOLOv8
 test_sample LPRNet
 test_sample DeepSORT
+test_sample ByteTrack
 test_sample OpenPose
 
 
