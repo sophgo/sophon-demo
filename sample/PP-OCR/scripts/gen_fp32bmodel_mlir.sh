ecdcc01f7c0d1ccb63b2f907b91c3b150ed87353
liheng.fang <liheng.fang@sophgo.com>
2023-05-15 21:21:24 +0800

    refactor(PP-OCR): refactor PP-OCR
    Change-Id: Ifb743c2f14f41bcc95505ad506d9c4573b8d6289

---
+++ sample/PP-OCR/scripts/gen_fp32bmodel_mlir.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+model_dir=$(cd `dirname $BASH_SOURCE[0]`/ && pwd)
+
+if [ ! $1 ]; then
+    target=bm1684x
+    target_dir=BM1684X
+else
+    target=${1,,}
+    target_dir=${target^^}
+fi
+
+outdir=../models/$target_dir
+
+function gen_mlir()
+{
+    model_transform.py \
+        --model_name ch_PP-OCRv3_det \
+        --model_def ../models/onnx/ch_PP-OCRv3_det.onnx \
+        --input_shapes [[$1,3,640,640]] \
+        --mlir ch_PP-OCRv3_det_$1b.mlir
+    model_transform.py \
+        --model_name ch_PP-OCRv3_cls \
+        --model_def ../models/onnx/ch_PP-OCRv3_cls.onnx \
+        --input_shapes [[$1,3,48,192]] \
+        --mlir ch_PP-OCRv3_cls_$1b.mlir
+    model_transform.py \
+        --model_name ch_PP-OCRv3_rec \
+        --model_def ../models/onnx/ch_PP-OCRv3_rec.onnx \
+        --input_shapes [[$1,3,48,320]] \
+        --mlir ch_PP-OCRv3_rec_$1b_320.mlir
+    model_transform.py \
+        --model_name ch_PP-OCRv3_rec \
+        --model_def ../models/onnx/ch_PP-OCRv3_rec.onnx \
+        --input_shapes [[$1,3,48,640]] \
+        --mlir ch_PP-OCRv3_rec_$1b_640.mlir
+    # model_transform.py \
+    #     --model_name ch_PP-OCRv3_rec \
+    #     --model_def ../models/onnx/ch_PP-OCRv3_rec.onnx \
+    #     --input_shapes [[$1,3,48,960]] \
+    #     --mlir ch_PP-OCRv3_rec_$1b_960.mlir
+}
+
+
+function gen_fp32bmodel()
+{
+    model_deploy.py \
+        --mlir ch_PP-OCRv3_det_$1b.mlir \
+        --quantize F32 \
+        --chip $target \
+        --model ch_PP-OCRv3_det_fp32_$1b.bmodel
+
+    mv ch_PP-OCRv3_det_fp32_$1b.bmodel $outdir/
+    
+    model_deploy.py \
+        --mlir ch_PP-OCRv3_cls_$1b.mlir \
+        --quantize F32 \
+        --chip $target \
+        --model ch_PP-OCRv3_cls_fp32_$1b.bmodel
+
+    mv ch_PP-OCRv3_cls_fp32_$1b.bmodel $outdir/
+
+    model_deploy.py \
+        --mlir ch_PP-OCRv3_rec_$1b_320.mlir \
+        --quantize F32 \
+        --chip $target \
+        --model ch_PP-OCRv3_rec_fp32_$1b_320.bmodel
+
+    mv ch_PP-OCRv3_rec_fp32_$1b_320.bmodel $outdir/
+
+    model_deploy.py \
+        --mlir ch_PP-OCRv3_rec_$1b_640.mlir \
+        --quantize F32 \
+        --chip $target \
+        --model ch_PP-OCRv3_rec_fp32_$1b_640.bmodel
+
+    mv ch_PP-OCRv3_rec_fp32_$1b_640.bmodel $outdir/
+
+    # model_deploy.py \
+    #     --mlir ch_PP-OCRv3_rec_$1b_960.mlir \
+    #     --quantize F32 \
+    #     --chip $target \
+    #     --model ch_PP-OCRv3_rec_fp32_$1b_960.bmodel
+
+    # mv ch_PP-OCRv3_rec_fp32_$1b_960.bmodel $outdir/
+}
+
+pushd $model_dir
+if [ ! -d $outdir ]; then
+    mkdir -p $outdir
+fi
+# batch_size=1
+gen_mlir 1
+gen_fp32bmodel 1
+
+# batch_size=4
+gen_mlir 4
+gen_fp32bmodel 4
+    
+echo "Combining bmodels..."
+model_tool --combine $outdir/ch_PP-OCRv3_det_fp32_*.bmodel -o $outdir/ch_PP-OCRv3_det_fp32.bmodel
+rm -r $outdir/ch_PP-OCRv3_det_fp32_*.bmodel
+model_tool --combine $outdir/ch_PP-OCRv3_cls_fp32_*.bmodel -o $outdir/ch_PP-OCRv3_cls_fp32.bmodel
+rm -r $outdir/ch_PP-OCRv3_cls_fp32_*.bmodel
+model_tool --combine $outdir/ch_PP-OCRv3_rec_fp32_*.bmodel -o $outdir/ch_PP-OCRv3_rec_fp32.bmodel
+rm -r $outdir/ch_PP-OCRv3_rec_fp32_*.bmodel
+
+popd
