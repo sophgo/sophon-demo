b7de25f48c04ce55f94ac01cff8cfa8d1d0abe08
liheng.fang <liheng.fang@sophgo.com>
2023-05-18 09:41:56 +0800

    fix(yolov5): improve cali script
    Change-Id: I2e5be07f891fbe2f83259315f7bfaf12c7755d45

--- sample/YOLOv5/scripts/gen_int8bmodel_nntc.sh
+++ sample/YOLOv5/scripts/gen_int8bmodel_nntc.sh
@@ -21,7 +21,7 @@ function auto_cali()
             --input_shapes="[1,3,640,640]"  \
             --target=$target   \
             --convert_bmodel_cmd_opt="-opt=1"   \
-            --try_cali_accuracy_opt="-fpfwd_outputs=< 24 >86,< 24 >55,< 24 >18;-th_method=ADMM"
+            --try_cali_accuracy_opt="-fpfwd_outputs=< 24 >86,< 24 >55,< 24 >18;-th_method=MAX"
     mv ../models/torch/yolov5s_batch1/compilation.bmodel $outdir/yolov5s_v6.1_3output_int8_1b.bmodel
 }
 
