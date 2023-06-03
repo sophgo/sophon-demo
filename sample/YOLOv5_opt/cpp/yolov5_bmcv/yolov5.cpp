f1b73017deee9b141d969b084c2c13f07b257676
liheng.fang <liheng.fang@sophgo.com>
2023-06-03 18:42:59 +0800

    fix(YOLOv5_opt): fix shape errors.
    Change-Id: Ie8c86596cf7ce21977c1f0b2b83d700d16f1fa32

--- sample/YOLOv5_opt/cpp/yolov5_bmcv/yolov5.cpp
+++ sample/YOLOv5_opt/cpp/yolov5_bmcv/yolov5.cpp
@@ -58,28 +58,6 @@ int YoloV5::Init(float confThresh, float nmsThresh, const std::string& tpu_kerne
   m_net_h = tensor->get_shape()->dims[2];
   m_net_w = tensor->get_shape()->dims[3];
 
-  const std::vector<std::vector<int>> supported_shapes{
-    {80, 160}, {160, 160}, {160, 320}, {160, 640}, {320, 320}, {320, 640}, {160, 960}, {320, 1280}, {640, 640}, {640, 1280}
-  }; //not all, if you want more, you can try yourself.(if failed, tpu may hang, you can use `sudo rmmod bmsophon && sudo modprobe bmsophon` to recover.)
-  bool input_valid_flag = false;
-  for(int i = 0; i < supported_shapes.size(); i++){
-    if(m_net_h == supported_shapes[i][0] && m_net_w == supported_shapes[i][1]){
-      input_valid_flag = true;
-      break;
-    }
-  }
-  if(input_valid_flag == false){
-    std::cerr << "Now the tpu postprocess function may not support the shape of your model, you can try it by adding such shape in `supported_shapes`."<<std::endl;
-    std::cerr << "(if failed, tpu may hang, you can use `sudo rmmod bmsophon && sudo modprobe bmsophon` to recover)" << std::endl;
-    std::cout << "Supported shapes: "; 
-    for(int i = 0; i < supported_shapes.size(); i++){
-      std::cout << "(" << supported_shapes[i][0] << ", " << supported_shapes[i][1] << "); ";
-    }
-    std::cout << std::endl;
-    exit(1);
-  }
-
-
   // 3. get output
   output_num = m_bmNetwork->outputTensorNum();
   assert(output_num == 1 || output_num == 3);
@@ -116,6 +94,10 @@ int YoloV5::Init(float confThresh, float nmsThresh, const std::string& tpu_kerne
   converto_attr.beta_2 = 0;
 
   // 6.tpukernel postprocess
+  if(m_net_h < 32 || m_net_w < 32 || m_net_h % 32 != 0 || m_net_w % 32 != 0 || m_net_h > 2048 || m_net_w > 2048 || (m_net_h + m_net_w) > 3072){
+    std::cerr << "Unsupported shape for tpukernel postprocession!" << std::endl;
+    exit(1);
+  }
   tpu_kernel_module_t tpu_module;
   tpu_module = tpu_kernel_load_module_file(m_bmContext->handle(), tpu_kernel_module_path.c_str()); 
   if(output_num == 3){
