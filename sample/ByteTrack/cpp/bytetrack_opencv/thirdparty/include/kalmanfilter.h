903707981d81fc5b62ebb05430453ca88ffb9830
yifan.wang <yifan.wang@sophgo.com>
2023-07-17 11:41:15 +0800

    feat(Bytetrack): add opencv version of bytetrack
    Change-Id: I9d76b4d9bcfb9f701441388fa04a5ea4cba1aa5c

---
+++ sample/ByteTrack/cpp/bytetrack_opencv/thirdparty/include/kalmanfilter.h
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// SOPHON-DEMO is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KALMANFILTER_H
+#define KALMANFILTER_H
+#include <opencv2/opencv.hpp>
+
+class KalmanFilter {
+ public:
+  static const double chi2inv95[10];
+  KalmanFilter();
+  ~KalmanFilter();
+  std::pair<cv::Mat, cv::Mat> initiate(const cv::Mat& measurement);
+  std::pair<cv::Mat, cv::Mat> predict(const cv::Mat& mean,
+                                      const cv::Mat& covariance);
+  std::pair<cv::Mat, cv::Mat> update(const cv::Mat& mean,
+                                     const cv::Mat& covariance,
+                                     const cv::Mat& measurement);
+  cv::Mat gating_distance(const cv::Mat& mean, const cv::Mat& covariance,
+                          const std::vector<cv::Mat>& measurements,
+                          bool only_position = false);
+
+ private:
+  cv::KalmanFilter* opencv_kf;
+  float _std_weight_position;
+  float _std_weight_velocity;
+};
+
+#endif  // KALMANFILTER_H
