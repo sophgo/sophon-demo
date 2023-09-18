a1b5547293dce3348e943ecf228318131ec2a4b0
liheng.fang <liheng.fang@sophgo.com>
2023-09-18 14:57:18 +0800

    fix(some demo): update download method sftp.
    Change-Id: I641c93a4c1cc0f129045feb58cb05f826dfc1311

--- sample/ResNet/scripts/download.sh
+++ sample/ResNet/scripts/download.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
-pip3 install dfn
-# sudo apt install unzip
-
+res=$(dpkg -l|grep unzip)
+if [ $? != 0 ];
+then
+    echo "Please install unzip on your system!"
+    exit
+fi
+pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 scripts_dir=$(dirname $(readlink -f "$0"))
-# echo $scripts_dir
 
 pushd $scripts_dir
-
 # datasets
 if [ ! -d "../datasets" ]; 
 then
-    mkdir -p ../datasets
-    # test dataset
-    # imagenet验证集随机抽取1000张
-    python3 -m dfn --url http://219.142.246.77:65000/sharing/oh2B5pGUu
-    unzip imagenet_val_1k.zip -d ../datasets/
-    rm imagenet_val_1k.zip
-
-    # cali data
-    # imagenet验证集随机抽出了200张
-    python3 -m dfn --url http://219.142.246.77:65000/sharing/W1ED8v06A
-    unzip cali_data.zip -d ../datasets/
-    rm cali_data.zip
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/ResNet/datasets_0918/datasets.zip
+    unzip datasets.zip -d ../
+    rm datasets.zip
 
     echo "datasets download!"
 else
-    echo "datasets exist!"
+    echo "Datasets folder exist! Remove it if you need to update."
 fi
 
 # models
 if [ ! -d "../models" ]; 
 then
-    # 包含原始pytorch模型、traced pytorch模型、fp32bmodel、fp16bmodel和int8bmodel
-    python3 -m dfn --url http://219.142.246.77:65000/sharing/dHWRblq7M
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/ResNet/models_0918/models.zip
     unzip models.zip -d ../
     rm models.zip
     echo "models download!"
 else
-    echo "models exist!"
+    echo "Models folder exist! Remove it if you need to update."
 fi
 popd
