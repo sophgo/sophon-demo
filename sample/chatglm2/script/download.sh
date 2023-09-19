ba1783f06166183acf1c3855adf6b78c4689b9c0
liheng.fang <liheng.fang@sophgo.com>
2023-09-19 11:51:17 +0800

    fix(chatglm2): update download method sftp
    Change-Id: Iae6b7395e5ffd539d814237f78f77fc654ef65bd

--- sample/chatglm2/script/download.sh
+++ sample/chatglm2/script/download.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-pip3 install zip
-pip3 install dfn
-# sudo apt install unzip
-
+res=$(dpkg -l|grep unzip)
+if [ $? != 0 ];
+then
+    echo "Please install unzip on your system!"
+    exit
+fi
+pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 scripts_dir=$(dirname $(readlink -f "$0"))
-# echo $scripts_dir
 
 pushd $scripts_dir
 # datasets
@@ -12,11 +14,14 @@ pushd $scripts_dir
 # models
 if [ ! -d "../models" ]; 
 then
-    python3 -m dfn --url http://disk-sophgo-vip.quickconnect.to/sharing/RYiN4At7v
-    unzip models.zip -d ../
-    rm models.zip
+    mkdir -p ../models/
+    mkdir -p ../models/BM1684X
+    pushd ../models/BM1684X
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/chatglm/models/BM1684X/chatglm2-6b.bmodel
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/chatglm/models/BM1684X/tokenizer.bmodel
+    popd
     echo "models download!"
 else
-    echo "models exist!"
+    echo "Models folder exist! Remove it if you need to update."
 fi
 popd
