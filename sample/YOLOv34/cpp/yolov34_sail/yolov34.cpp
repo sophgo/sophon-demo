bcbcf4d2d7f9606e222591984adb721ab1185443
ruohao.wu <ruohao.wu@sophgo.com>
2023-11-23 15:30:12 +0800

    fix(YOLOv34): adapt BM1688
    Change-Id: I2db0e76e53e932656836f087e6e63a53ae95ddb2

--- sample/YOLOv34/cpp/yolov34_sail/yolov34.cpp
+++ sample/YOLOv34/cpp/yolov34_sail/yolov34.cpp
@@ -589,7 +589,13 @@ void Yolo::draw_bmcv(int classId,
     int crop_h = MAX(MIN(height, frame.height() - top), 0);
     auto color_tuple = std::make_tuple(colors[classId % colors_num][2], colors[classId % colors_num][1],
                                        colors[classId % colors_num][0]);
-    bmcv->rectangle(frame, start_x, start_y, crop_w, crop_h, color_tuple, 3);
+    int thickness = 2;
+    if(width < thickness * 2 || height < thickness * 2){
+        std::cout << "width or height too small, this rect will not be drawed: " << 
+              "[" << start_x << ", "<< start_y << ", " << crop_w << ", " << crop_h << "]" << std::endl;
+    } else{
+        bmcv->rectangle(frame, start_x, start_y, crop_w, crop_h, color_tuple, thickness);
+    }
     if (put_text_flag) {  // only support YUV420P, puttext not used here.
         std::string label = m_class_names[classId] + ":" + cv::format("%.2f", conf);
         if (BM_SUCCESS != bmcv->putText(frame, label.c_str(), left, top, color_tuple, 2, 2)) {
