0f86a6f74513fed2fa1ca4db722027fb9d18cf64
liheng.fang <liheng.fang@sophgo.com>
2023-12-08 10:30:21 +0800

    fix(SSD): fix download script.
    Change-Id: Iedb1bcc518affa6dfaddc63d78e412c7f6da7bf0

--- sample/SSD/scripts/download.sh
+++ sample/SSD/scripts/download.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+res=$(which unzip)
+if [ $? != 0 ];
+then
+    echo "Please install unzip on your system!"
+    exit
+fi
 scripts_dir=$(dirname $(readlink -f "$0"))
 # echo $scripts_dir
 # sudo apt-get install zip
-pip3 install dfn
+pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 pushd $scripts_dir
-python3 -m dfn --url http://219.142.246.77:65000/sharing/WWGkMQaca
-unzip data.zip -d ../
-rm data.zip
+python3 -m dfss --url=open@sophgo.com:sophon-demo/SSD/data.zip
+# unzip data.zip -d ../
+# rm data.zip
 echo "All done!"
 popd
