c88aa26271b2748269d516a47dc08fa0577eeb63
liheng.fang <liheng.fang@sophgo.com>
2024-04-16 17:21:01 +0800

    fix(ppYoloe): fix download.sh.
    Change-Id: I8696b2101b17c0998a5875071be173ae0a1cafe8
    (cherry picked from commit ae0ad20dc2040d994dcff77ff0f41dd9051a3aee)

--- sample/ppYoloe/scripts/download.sh
+++ sample/ppYoloe/scripts/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip3 install dfss
+pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 
 res=$(which unzip)
 if [ $? != 0 ];
@@ -7,6 +7,14 @@ then
     echo "Please install unzip on your system!"
     echo "To install, use the following command:"
     echo "sudo apt install unzip"
+    exit
+fi
+res=$(which tar)
+if [ $? != 0 ];
+then
+    echo "Please install tar on your system!"
+    echo "To install, use the following command:"
+    echo "sudo apt install tar"
     exit
 fi
 
@@ -34,6 +42,13 @@ then
     python3 -m dfss --url=open@sophgo.com:sophon-demo/ppYoloe/models.zip
     unzip models.zip -d ../models
     rm models.zip
+    pushd ../models/
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/ppYoloe/models_20240416/BM1688.tgz
+    tar xvf BM1688.tgz
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/ppYoloe/models_20240416/CV186X.tgz
+    tar xvf CV186X.tgz
+    rm -r *.tgz
+    popd
     echo "models download!"
 else
     echo "models exist!"
