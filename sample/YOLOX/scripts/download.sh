adb0ffb553048b8aac0fe19f0ea11d728a321efd
wangyihu <yihu.wang@sophgo.com>
2024-05-15 14:08:57 +0800

    feat(YOLOX): add cv186x support.
    Change-Id: I9adcc7050a9f06d0ff47564a9c5c161e6279899

--- sample/YOLOX/scripts/download.sh
+++ sample/YOLOX/scripts/download.sh
@@ -24,6 +24,11 @@ then
     python3 -m dfss --url=open@sophgo.com:sophon-demo/YOLOX/models_1220/models.zip
     unzip models.zip -d ../
     rm models.zip
+
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/YOLOX/models_20240422/CV186X_models.tar.gz
+    tar -zxvf CV186X_models.tar.gz -C ../
+    rm CV186X_models.tar.gz
+
     echo "models download!"
 else
     echo "models exist!"
