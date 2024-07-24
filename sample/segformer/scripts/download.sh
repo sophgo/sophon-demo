171d5fc960ccc9f90652c36234e71825ffed31e2
qi.zhang <qi.zhang@sophgo.com>
2024-07-24 18:21:30 +0800

    feat(segformer): 1.Support BM1688/CV186X. 2.Generate benchmarks in auto_test.
    Change-Id: I7ef3aab5195e830ebec7f891596d229839cf0356

--- sample/segformer/scripts/download.sh
+++ sample/segformer/scripts/download.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-res=$(which unzip)
-if [ $? != 0 ];
-then
-    echo "Please install unzip on your system!"
-    exit
-fi
 pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 script_dir=$(dirname $(readlink -f "$0"))
 echo $script_dir
@@ -14,28 +8,40 @@ pushd $script_dir
 # datasets
 if [ ! -d "../datasets" ];
 then
-    mkdir -p ../datasets
-    # test dataset
-    # test input_test
-    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/datasets.zip
-    unzip datasets.zip -d ../datasets
-    rm datasets.zip
+    mkdir ../datasets
+    pushd ../datasets
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/datasets/cali.tar.gz                       
+    tar xvf cali.tar.gz && rm cali.tar.gz                                   
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/datasets/cityscapes.tar.gz    
+    tar xvf cityscapes.tar.gz && rm cityscapes.tar.gz
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/datasets/cityscapes_small.tar.gz 
+    tar xvf cityscapes_small.tar.gz && rm cityscapes_small.tar.gz
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/datasets/test_car_person_1080P.mp4         
+    popd
     echo "datasets download!"
 else
-    echo "datasets exist!"
+    echo "Datasets folder exist! Remove it if you need to update."
 fi
 
 
 # models
 if [ ! -d "../models" ]; 
 then
-    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/models.zip
-    ls -al
-    unzip models.zip -d ../
-    rm models.zip
+    mkdir ../models
+    pushd ../models
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/models/BM1684.tar.gz    
+    tar xvf BM1684.tar.gz && rm BM1684.tar.gz 
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/models/BM1684X.tar.gz    
+    tar xvf BM1684X.tar.gz && rm BM1684X.tar.gz 
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/models/BM1688.tar.gz     
+    tar xvf BM1688.tar.gz  && rm BM1688.tar.gz  
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/models/CV186X.tar.gz    
+    tar xvf CV186X.tar.gz && rm CV186X.tar.gz 
+    python3 -m dfss --url=open@sophgo.com:sophon-demo/segformer/models/onnx.tar.gz    
+    tar xvf onnx.tar.gz && rm onnx.tar.gz 
+    popd
     echo "models download!"
 else
-    echo "models exist!"
+    echo "Models folder exist! Remove it if you need to update."
 fi
-
 popd
