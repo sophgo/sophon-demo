5764f2a38b5fea788ec1b525bcf6916d907ccf48
liheng.fang <liheng.fang@sophgo.com>
2024-09-06 21:22:08 +0800

    refactor(all): Remove all samples' outside dependencies.
    Change-Id: Ibfd16a58f168deb2a7e242eab8fda63fc1517231

---
+++ sample/ByteTrack/python/detector/yolov5/utils.py
@@ -0,0 +1,124 @@
+#===----------------------------------------------------------------------===#
+#
+# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+#
+# SOPHON-DEMO is licensed under the 2-Clause BSD License except for the
+# third-party components.
+#
+#===----------------------------------------------------------------------===#
+import os
+import sys
+import numpy as np
+import cv2
+
+COCO_CLASSES = ('background', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+                'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+                'scissors', 'teddy bear', 'hair drier', 'toothbrush')
+
+COLORS = [
+        [56, 0, 255],
+        [226, 255, 0],
+        [0, 94, 255],
+        [0, 37, 255],
+        [0, 255, 94],
+        [255, 226, 0],
+        [0, 18, 255],
+        [255, 151, 0],
+        [170, 0, 255],
+        [0, 255, 56],
+        [255, 0, 75],
+        [0, 75, 255],
+        [0, 255, 169],
+        [255, 0, 207],
+        [75, 255, 0],
+        [207, 0, 255],
+        [37, 0, 255],
+        [0, 207, 255],
+        [94, 0, 255],
+        [0, 255, 113],
+        [255, 18, 0],
+        [255, 0, 56],
+        [18, 0, 255],
+        [0, 255, 226],
+        [170, 255, 0],
+        [255, 0, 245],
+        [151, 255, 0],
+        [132, 255, 0],
+        [75, 0, 255],
+        [151, 0, 255],
+        [0, 151, 255],
+        [132, 0, 255],
+        [0, 255, 245],
+        [255, 132, 0],
+        [226, 0, 255],
+        [255, 37, 0],
+        [207, 255, 0],
+        [0, 255, 207],
+        [94, 255, 0],
+        [0, 226, 255],
+        [56, 255, 0],
+        [255, 94, 0],
+        [255, 113, 0],
+        [0, 132, 255],
+        [255, 0, 132],
+        [255, 170, 0],
+        [255, 0, 188],
+        [113, 255, 0],
+        [245, 0, 255],
+        [113, 0, 255],
+        [255, 188, 0],
+        [0, 113, 255],
+        [255, 0, 0],
+        [0, 56, 255],
+        [255, 0, 113],
+        [0, 255, 188],
+        [255, 0, 94],
+        [255, 0, 18],
+        [18, 255, 0],
+        [0, 255, 132],
+        [0, 188, 255],
+        [0, 245, 255],
+        [0, 169, 255],
+        [37, 255, 0],
+        [255, 0, 151],
+        [188, 0, 255],
+        [0, 255, 37],
+        [0, 255, 0],
+        [255, 0, 170],
+        [255, 0, 37],
+        [255, 75, 0],
+        [0, 0, 255],
+        [255, 207, 0],
+        [255, 0, 226],
+        [255, 245, 0],
+        [188, 255, 0],
+        [0, 255, 18],
+        [0, 255, 75],
+        [0, 255, 151],
+        [255, 56, 0],
+        [245, 255, 0],
+    ]
+
+def is_img(file_name):
+    """judge the file is available image or not
+    Args:
+        file_name (str): input file name
+    Returns:
+        (bool) : whether the file is available image
+    """
+    fmt = os.path.splitext(file_name)[-1]
+    if isinstance(fmt, str) and fmt.lower() in ['.jpg','.png','.jpeg','.bmp','.jpeg','.webp']:
+        return True
+    else:
+        return False
