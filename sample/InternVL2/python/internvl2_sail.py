dbe20aca07307f6b9e9c83b5a236cbeea90676aa
liheng.fang <liheng.fang@sophgo.com>
2025-01-13 16:52:31 +0800

    feat(InternVL2): Add dynamic preprocess.
    Change-Id: I7c8132416f484ec6b2cc6d51cdffe5a4e4cc12ba

--- sample/InternVL2/python/internvl2_sail.py
+++ sample/InternVL2/python/internvl2_sail.py
@@ -26,13 +26,66 @@ def build_transform(input_size):
     ])
     return transform
 
+def find_closest_aspect_ratio(aspect_ratio, target_ratios, width, height, image_size):
+    best_ratio_diff = float('inf')
+    best_ratio = (1, 1)
+    area = width * height
+    for ratio in target_ratios:
+        target_aspect_ratio = ratio[0] / ratio[1]
+        ratio_diff = abs(aspect_ratio - target_aspect_ratio)
+        if ratio_diff < best_ratio_diff:
+            best_ratio_diff = ratio_diff
+            best_ratio = ratio
+        elif ratio_diff == best_ratio_diff:
+            if area > 0.5 * image_size * image_size * ratio[0] * ratio[1]:
+                best_ratio = ratio
+    return best_ratio
+
+def dynamic_preprocess(image, min_num=1, max_num=12, image_size=448, use_thumbnail=False):
+    orig_width, orig_height = image.size
+    aspect_ratio = orig_width / orig_height
+
+    # calculate the existing image aspect ratio
+    target_ratios = set(
+        (i, j) for n in range(min_num, max_num + 1) for i in range(1, n + 1) for j in range(1, n + 1) if
+        i * j <= max_num and i * j >= min_num)
+    target_ratios = sorted(target_ratios, key=lambda x: x[0] * x[1])
+
+    # find the closest aspect ratio to the target
+    target_aspect_ratio = find_closest_aspect_ratio(
+        aspect_ratio, target_ratios, orig_width, orig_height, image_size)
+
+    # calculate the target width and height
+    target_width = image_size * target_aspect_ratio[0]
+    target_height = image_size * target_aspect_ratio[1]
+    blocks = target_aspect_ratio[0] * target_aspect_ratio[1]
+
+    # resize the image
+    resized_img = image.resize((target_width, target_height))
+    processed_images = []
+    for i in range(blocks):
+        box = (
+            (i % (target_width // image_size)) * image_size,
+            (i // (target_width // image_size)) * image_size,
+            ((i % (target_width // image_size)) + 1) * image_size,
+            ((i // (target_width // image_size)) + 1) * image_size
+        )
+        # split the image
+        split_img = resized_img.crop(box)
+        processed_images.append(split_img)
+    assert len(processed_images) == blocks
+    if use_thumbnail and len(processed_images) != 1:
+        thumbnail_img = image.resize((image_size, image_size))
+        processed_images.append(thumbnail_img)
+    return processed_images
 
 def load_image(image_file, input_size=448, max_num=12):
     image = Image.open(image_file).convert('RGB')
     transform = build_transform(input_size=input_size)
-    pixel_values = transform(image)
+    images = dynamic_preprocess(image, max_num=max_num, image_size=input_size, use_thumbnail=True)
+    pixel_values = [transform(image) for image in images]
+    pixel_values = torch.stack(pixel_values)
     return pixel_values
-
 
 class InternVL2():
     def __init__(self, args):
@@ -322,7 +375,7 @@ class InternVL2():
             self.pixel_values = []
             return
         self.pixel_values = load_image(
-            self.image_str, max_num=1).unsqueeze(0)
+            self.image_str, max_num=1)
         system_ids = self.tokenizer.encode(self.system_prompt + "<img>")
         self.image_offset = len(system_ids)
         prompt_ids = self.tokenizer.encode(
