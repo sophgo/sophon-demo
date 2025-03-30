ea8ef18d9953b53927901f9926c04fab1e7a834e
liheng.fang <liheng.fang@sophgo.com>
2025-03-30 15:25:40 +0800

    fix(YOLOv8_series): Remove duplicated code.
    Change-Id: If318614cf93c6959693ec5b03377c21bc9dccbc6

--- sample/YOLOv8_plus_det/cpp/yolov8_bmcv/yolov8_det.cpp
+++ sample/YOLOv8_plus_det/cpp/yolov8_bmcv/yolov8_det.cpp
@@ -402,13 +402,6 @@ int YoloV8_det::post_process(const std::vector<bm_image>& input_images,
                 bm_status_t ret = bm_mem_unmap_device_mem(handle, tensor_data, tensor_size);
                 assert(BM_SUCCESS == ret);
             }
-            if(output_tensors[i].dtype != BM_FLOAT32){
-                delete [] tensor_data;
-            } else {
-                int tensor_size = bm_mem_get_device_size(output_tensors[i].device_mem);
-                bm_status_t ret = bm_mem_unmap_device_mem(handle, tensor_data, tensor_size);
-                assert(BM_SUCCESS == ret);
-            }
         } else {
             delete [] tensor_data;
         }
