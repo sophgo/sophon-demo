20bc6e2c9a9752e76b4661b62cf9a7b947068303
haibo.wan <haibo.wan@sophgo.com>
2025-07-03 14:52:51 +0800

    add(MiniCPM4): add MiniCPM4 python sample
    Change-Id: I924c9fb412d7e4e65ebe7deba4e9c234b1a0067a
    (cherry picked from commit bedc8e79fcc140f4a848871542a3abd64726859e)

---
+++ sample/MiniCPM4/scripts/download.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
+scripts_dir=$(dirname $(readlink -f "$0"))
+
+pushd $scripts_dir
+
+function download_bm1684x {
+    if [ ! -d "../models/BM1684X" ]; then
+        mkdir -p ../models/BM1684X
+    fi
+    pushd ../models/BM1684X
+        python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/minicpm4-8b_w4bf16_seq512_bm1684x_1dev_20250613_175044.bmodel
+        python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU/minicpm4-8b_w4bf16_seq8192_bm1684x_1dev_20250613_182940.bmodel
+    popd
+
+    pushd ../python/token_config
+        python3 -m dfss --url=open@sophgo.com:sophon-demo/MiniCPM4/tokenizer.model
+    popd
+}
+
+function download_bm1688 {
+    if [ ! -d "../models/BM1688" ]; then
+        mkdir -p ../models/BM1688
+    fi
+    pushd ../models/BM1688
+        # minicpm4-0.5b bm1688 512
+        python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU_Lite/minicpm4-0.5b-gptq_w4bf16_seq512_bm1688_2core_20250616_122001.bmodel
+    popd
+
+    pushd ../python/token_config
+        python3 -m dfss --url=open@sophgo.com:sophon-demo/MiniCPM4/tokenizer.model
+    popd
+}
+
+function download_cv186x {
+    if [ ! -d "../models/CV186X" ]; then
+        mkdir -p ../models/CV186X
+    fi
+    pushd ../models/BCV186X
+        # minicpm4-0.5b cv186x 512
+        python3 -m dfss --url=open@sophgo.com:/ext_model_information/LLM/LLM-TPU_Lite/minicpm4-0.5b-gptq_w4bf16_seq512_cv186x_1core_20250616_122126.bmodel
+    popd
+
+    pushd ../python/token_config
+        python3 -m dfss --url=open@sophgo.com:sophon-demo/MiniCPM4/tokenizer.model
+    popd
+}
+
+
+
+if [ "$1" == "bm1684x" ]; then
+    download_bm1684x
+else if [ "$1" == "bm1688" ]; then
+    download_bm1688
+else if [ "$1" == "cv186ah" ]; then
+    download_cv186ah
+else
+    echo "Error Parameter"
+    echo "Usage: $0 [bm1684x|bm1688|cv186ah]"
+    exit 1
+fi
