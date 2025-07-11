d323e6c907768bdec9377cd04f450ea3c5c8db91
liheng.fang <liheng.fang@sophgo.com>
2025-07-11 10:17:45 +0800

    fix(whisper): bugfix.
    Change-Id: Ia924061c35b22441416f4f8699555fcea2b7efd2
    (cherry picked from commit 8a8df163011635ed0c5d7c1866e33e33cb09774d)

--- sample/Whisper/python/bmwhisper/model.py
+++ sample/Whisper/python/bmwhisper/model.py
@@ -108,7 +108,7 @@ class Whisper():
             kvcache_rearrange_input_tensors_map = self.kvcache_rearrange_input_tensors_map
             kvcache_rearrange_output_tensors_map = self.kvcache_rearrange_output_tensors_map
             
-            kvcache_rearrange_input_tensors_map[0] = self.decoder_main_output_tensors_map[i]
+            kvcache_rearrange_input_tensors_map[0] = self.decoder_main_output_tensors_map[i + 1]
 
             kvcache_rearrange_output_tensors_map[0] = kvcache_rearrange_input_tensors_map[0]
 
