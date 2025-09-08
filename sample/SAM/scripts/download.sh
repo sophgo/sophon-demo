913adf01d4abe498543f8b49508abe2a59fc201f
liheng.fang <liheng.fang@sophgo.com>
2025-09-08 16:03:54 +0800

    feat(SAM): : add mobile_sam.
    Change-Id: I748f578170f2261aeb9963ddcd224803b45da475
    (cherry picked from commit d90655ed2f024b73e60f86f8c88cd5d00f76bfe7)

--- sample/SAM/scripts/download.sh
+++ sample/SAM/scripts/download.sh
@@ -5,6 +5,13 @@ then
     echo "Please install unzip on your system!"
     exit
 fi
+
+if [ ! $2 ]; then
+    model_type=SAM-ViT-B
+else
+    model_type=$2
+fi
+
 pip3 install dfss -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade
 scripts_dir=$(dirname $(readlink -f "$0"))
 
@@ -24,9 +31,17 @@ fi
 # models
 if [ ! -d "../models" ]; 
 then
-    python3 -m dfss --url=open@sophgo.com:sophon-demo/SAM/models.zip
-    unzip models.zip -d ../
-    rm models.zip
+    echo "downloading model_type: $model_type."
+    if test $model_type = "SAM-ViT-B"; then
+        python3 -m dfss --url=open@sophgo.com:sophon-demo/SAM/models.zip
+        unzip models.zip -d ../
+        rm models.zip
+    elif test $model_type = "SAM-ViT-T"; then
+        python3 -m dfss --url=open@sophgo.com:sophon-demo/SAM/mobile_models.tar.gz
+        tar xvf mobile_models.tar.gz -C ../ && rm mobile_models.tar.gz
+    else
+        echo "unsupport model_type: $model_type, only supports: SAM-ViT-B,SAM-ViT-T."
+    fi
     echo "models download!"
 else
     echo "Models folder exist! Remove it if you need to update."
