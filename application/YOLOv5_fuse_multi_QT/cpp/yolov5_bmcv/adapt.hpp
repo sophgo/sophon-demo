d07f3037f6777f87febd299508033ef79ff83c9e
liheng.fang <liheng.fang@sophgo.com>
2025-12-24 11:43:31 +0800

    fix(YOLOv5_fuse_multi_QT): fix adapt.hpp.
    Change-Id: I35f1bb640ed9e75d5eca3c6893ad38655871ce9b

--- application/YOLOv5_fuse_multi_QT/cpp/yolov5_bmcv/adapt.hpp
+++ application/YOLOv5_fuse_multi_QT/cpp/yolov5_bmcv/adapt.hpp
@@ -18,9 +18,6 @@ static inline bm_status_t bm_image_destroy(bm_image& image){
   return bm_image_destroy(&image);
 }
 typedef bmcv_padding_attr_t bmcv_padding_atrr_t;
-static inline bm_status_t bm_image_dettach_contiguous_mem(int image_num, bm_image *images){
-  return bm_image_detach_contiguous_mem(image_num, images);
-}
 #endif
 
 #endif
