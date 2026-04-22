632fa868f48752341dff9570864e8c4068a3be53
liheng.fang <liheng.fang@sophgo.com>
2026-04-22 14:16:27 +0800

    add(LFM2-VL): add new sample.
    Change-Id: I44fb3ca8afc1f265901e7fb3feb4fdc072a657a5

---
+++ sample/LFM2-VL/cpp/README.md
@@ -0,0 +1,82 @@
+[简体中文](./README.md)
+
+# C++例程
+
+## 目录
+
+- [C++例程](#c例程)
+  - [目录](#目录)
+  - [1. 环境准备](#1-环境准备)
+    - [1.1 x86/arm/riscv PCIe平台](#11-x86armriscv-pcie平台)
+    - [1.2 SoC平台](#12-soc平台)
+  - [2. 程序编译](#2-程序编译)
+  - [3. 推理测试](#3-推理测试)
+    - [3.1 参数说明](#31-参数说明)
+  - [4. Tokenizer编译方法](#4-tokenizer编译方法)
+
+
+## 1. 环境准备
+### 1.1 x86/arm/riscv PCIe平台
+如果您在x86/arm平台安装了PCIe加速卡（如SC系列加速卡），可以直接使用它作为开发环境和运行环境。您需要安装libsophon，具体步骤可参考[x86-pcie平台的开发和运行环境搭建](../../../docs/Environment_Install_Guide.md#3-x86-pcie平台的开发和运行环境搭建)或[arm-pcie平台的开发和运行环境搭建](../../../docs/Environment_Install_Guide.md#5-arm-pcie平台的开发和运行环境搭建)或[riscv-pcie平台的开发和运行环境搭建](../../../docs/Environment_Install_Guide.md#6-riscv-pcie平台的开发和运行环境搭建)。
+
+### 1.2 SoC平台
+如果您使用SoC平台（如SE、SM系列边缘设备），刷机后在`/opt/sophon/`下已经预装了相应的libsophon运行库包，可直接使用它作为运行环境。通常还需要一台x86主机作为开发环境，用于交叉编译C++程序。
+
+
+## 2. 程序编译
+C++程序运行前需要编译可执行文件，您无需交叉编译，完成环境准备后，您可以在x86/aarch64等架构的平台上直接编译可执行程序。
+```bash
+cd cpp
+mkdir build && cd build
+cmake ..
+make
+cd ..
+```
+编译完成后，会在build目录下生成./pipeline。
+
+## 3. 推理测试
+对于PCIe平台，可以直接在PCIe平台上推理测试；对于SoC平台，需将交叉编译生成的可执行文件及所需的模型、测试数据拷贝到SoC平台中测试。测试的参数及运行方式是一致的，下面主要以PCIe模式进行介绍。
+
+### 3.1 参数说明
+可执行程序默认有一套参数，请注意根据实际情况进行传参，以qwen_bmlib.pcie为例，具体参数说明如下：
+```bash
+Usage:
+  -h, --help        : Show help info 
+  -m, --model       : Set model path 
+  -c, --config      : Set config path 
+  -s, --do_sample   : Enable sampling during generation
+  -d, --devid       : Set devices to run for model, default is '0'
+
+### 3.2 测试
+
+需要在`cpp/build`目录下执行程序，测试实例如下。
+```bash
+./pipeline --model ../../models/lfm2-vl-1.6b_w4bf16_seq1024_bm1688_1core_static_20260422_145102.bmodel --config ../../configs/
+```
+运行此命令，进入对话后，需要输入mode、image path，然后模型会给出回答。
+
+## 4. Tokenizer编译方法
+
+本例程的下载脚本提供了编译好的tokenizer，如果您需要自己编译，可以参考以下步骤：
+
+- 下载源码
+```bash
+git clone https://github.com/mlc-ai/tokenizers-cpp
+cd tokenizers-cpp
+```
+
+- 升级RUST编译工具包
+```bash
+export RUSTUP_DIST_SERVER="https://rsproxy.cn"
+export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
+curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh | sh
+```
+
+- 直接在目标平台编译
+```bash
+mkdir build && cd build
+cmake ..
+make
+```
+
+- 拷贝静态库和头文件`build/libtokenizers_c.a`、`build/libtokenizers_cpp.a`、`build/sentencepiece/libsentencepiece.a`、`build/release/libtokenizers_c.a`和`/include`到工作目录，即可
---
+++ sample/LFM2-VL/cpp/chat.hpp
@@ -0,0 +1,122 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2025 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "bmruntime_interface.h"
+#include "memory.h"
+#include <algorithm>
+#include <assert.h>
+#include <chrono>
+#include <cstdlib>
+#include <getopt.h>
+#include <inttypes.h>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <stdio.h>
+#include <vector>
+
+class LFM2_VL {
+public:
+  void init(int devid, std::string model_path, std::string config_path = "",
+            bool do_sample = false);
+  void deinit();
+  void forward_embed(std::vector<int> const &tokens);
+  void forward_vit(const float *pixel_values, int vit_offset);
+  int forward_first(std::vector<int> const &position_ids);
+  int forward_next(std::vector<int> const &position_ids);
+  bool check_stop(const std::string &text);
+  void clear_history();
+
+  std::mt19937 sgen;
+  LFM2_VL() : sgen(std::random_device()()) {};
+
+private:
+  void net_launch(const bm_net_info_t *net,
+                  const std::vector<bm_tensor_t> &in_tensors,
+                  std::vector<bm_tensor_t> &out_tensors);
+  void net_launch_full_attention(int block_idx, int idx_kv, int kv_offset,
+                         bm_device_mem_t &input_mem, const int *position_id,
+                         std::vector<uint16_t> &attention_mask);
+  inline void d2d(bm_device_mem_t &dst, bm_device_mem_t &src, int offset = 0,
+                  int size = 0);
+  void init_by_names();
+  int forward_first_with_kv(std::vector<int> const &position_ids);
+  int generate(bm_device_mem_t &logits_mem);
+  int greedy_search(bm_device_mem_t &logits_mem);
+  int penalty_sample(bm_device_mem_t &logits_mem);
+  void init_tensors(const bm_net_info_t *net,
+                    std::vector<bm_tensor_t> &in_tensors,
+                    std::vector<bm_tensor_t> &out_tensors, int stage = 0);
+
+public:
+  int token_length;
+  int history_length;
+  int SEQLEN;
+  int MAX_INPUT_LENGTH;
+  int PREFILL_KV_LENGTH;
+  int HIDDEN_SIZE;
+  int KV_BYTES = -1; // kv bytes for one token
+  int CONV_STATE_BYTES = -1; // conv state bytes for one token
+  int NUM_LAYERS;
+  int VIT_DIMS;
+  int TOKENS_PER_TILE;
+  int MAX_PATCHES;
+  int MAX_PIXELS;
+  int max_pos;
+  bool lmhead_with_topk;
+  bool support_history;
+  bool is_dynamic;
+  bool vit_dynamic;
+  uint16_t mask_value;
+  bool vit_run = false;
+  std::vector<int> visited_tokens;
+  std::vector<int> VIT_PATCH_LIST;
+  std::vector<int> INPUT_LENGTH_LIST;
+  bool do_sample = false;
+  // generation
+  std::vector<std::string> stop_strings;
+  float penalty;
+  float temperature;
+  int top_k;
+  float top_p;
+  const int conv_L_cache = 3;
+
+private:
+  bm_handle_t bm_handle;
+  void *p_bmrt;
+  std::vector<const bm_net_info_t *> net_blocks;
+  std::vector<const bm_net_info_t *> net_blocks_cache;
+  const bm_net_info_t *net_embed;
+  const bm_net_info_t *net_embed_cache;
+  const bm_net_info_t *net_lm;
+  const bm_net_info_t *net_vit;
+  const bm_net_info_t *net_greedy_head, *net_sample_head;
+  bm_device_mem_t dev_buffer;
+  std::vector<bm_device_mem_t> past_key;
+  std::vector<bm_device_mem_t> past_value;
+  std::vector<bm_device_mem_t> past_conv_state;
+  const std::vector<std::string> layer_types = {
+      "conv",
+      "conv",
+      "full_attention",
+      "conv",
+      "conv",
+      "full_attention",
+      "conv",
+      "conv",
+      "full_attention",
+      "conv",
+      "full_attention",
+      "conv",
+      "full_attention",
+      "conv",
+      "full_attention",
+      "conv"
+  };
+};
