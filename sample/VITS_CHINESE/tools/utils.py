27c0790f18e609277ca4abf3e5495a18b40b6aa9
liheng.fang <liheng.fang@sophgo.com>
2026-04-24 16:49:58 +0800

    fix(all): fix open function usage.
    Change-Id: Ia690bb46aa89468877912d7bbd8f96cf66143252

--- sample/VITS_CHINESE/tools/utils.py
+++ sample/VITS_CHINESE/tools/utils.py
@@ -296,7 +296,8 @@ def check_git_hash(model_dir):
 
     path = os.path.join(model_dir, "githash")
     if os.path.exists(path):
-        saved_hash = open(path).read()
+        with open(path) as f:
+            saved_hash = f.read()
         if saved_hash != cur_hash:
             logger.warn(
                 "git hash values are different. {}(saved) != {}(current)".format(
@@ -304,7 +305,8 @@ def check_git_hash(model_dir):
                 )
             )
     else:
-        open(path, "w").write(cur_hash)
+        with open(path, "w") as f:
+            f.write(cur_hash)
 
 
 def get_logger(model_dir, filename="train.log"):
